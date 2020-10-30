eb8e9dbcaeecb7a943968b6219e8d49ad7237d85
Alexandre Szilagyi Santos <alexandre.szilagyi.santoz@gmail.com>
2020-10-30 02:31:16 -0400

    First Commit

---
+++ timer.js
@@ -0,0 +1,60 @@
+class Timer {
+  constructor(durationInput, startBtn, pauseBtn, callbacks) {
+    this.durationInput = durationInput;
+    this.startBtn = startBtn;
+    this.pauseBtn = pauseBtn;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+      this.onInvalidInput = callbacks.onInvalidInput;
+    }
+
+    this.durationInput.addEventListener("input", this.pause);
+    this.startBtn.addEventListener("click", this.start);
+    this.pauseBtn.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    if (isNaN(this.timeRemaining)) {
+      this.onInvalidInput();
+    } else {
+      if (this.onStart) {
+        this.onStart(this.timeRemaining);
+      }
+      if (this.interval > 0) {
+        this.pause();
+        this.interval = undefined;
+      } else {
+        this.tick();
+        this.interval = setInterval(this.tick, 10);
+      }
+    }
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 0.01;
+      if (this.onTick) {
+        this.onTick(this.timeRemaining);
+      }
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time.toFixed(2);
+  }
+}
